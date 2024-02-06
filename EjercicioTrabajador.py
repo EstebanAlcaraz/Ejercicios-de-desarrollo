11bc5f684f0906e51f12ad4556c4483958cd2520
Esteban Alcaraz <estebanalfa12@hotmail.com>
2024-02-06 12:06:11 -0500

    C2 extra

--- EjercicioTrabajador.py
+++ EjercicioTrabajador.py
@@ -5,3 +5,9 @@ Salario = float(input("Ingrese el salario del empleado"))
 Horas_extras = float(input("Ingrese las horas extras trabajadas"))
 valor_hora = (Salario*0.35) + Salario
 Total_Hextras = Horas_extras*valor_hora
+
+#C2 Extras
+Horas_trabajadas = float(input("Ingrese las horas normales trabajadas"))
+valor_pagado = Horas_trabajadas * valor_hora
+neto_pagar = Total_Hextras + valor_pagado
+total_horas_trabajadas = Horas_trabajadas + Horas_extras
