0aa4c524f7fcfa297616f9a369e7a57fc5d02ea0
Esteban Alcaraz <estebanalfa12@hotmail.com>
2024-02-06 12:08:02 -0500

    C3 pagos

--- EjercicioTrabajador.py
+++ EjercicioTrabajador.py
@@ -11,3 +11,9 @@ Horas_trabajadas = float(input("Ingrese las horas normales trabajadas"))
 valor_pagado = Horas_trabajadas * valor_hora
 neto_pagar = Total_Hextras + valor_pagado
 total_horas_trabajadas = Horas_trabajadas + Horas_extras
+
+#C3 pagos
+print("El trabajador ", Nombre, "Realizo ", Horas_trabajadas, "horas normales, ", "y", Horas_extras, "horas extras, para un total de ", total_horas_trabajadas, "Horas")
+print("El salario base del trabajador es: ", Salario, "y por las horas laborales realizadas su ingreso total es de: ", neto_pagar)
+
+#C4 Fin
