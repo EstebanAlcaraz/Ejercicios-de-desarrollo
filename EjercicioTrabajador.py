bb2d83235811441bda538bedb8784eb16ee8a049
Esteban Alcaraz <estebanalfa12@hotmail.com>
2024-02-06 11:52:19 -0500

    C1 inicioo

---
+++ EjercicioTrabajador.py
@@ -0,0 +1,7 @@
+Nombre = str(input("Ingrese el nombre del empleado"))
+Salario = float(input("Ingrese el salario del empleado"))
+
+#C1 Inicio
+Horas_extras = float(input("Ingrese las horas extras trabajadas"))
+valor_hora = (Salario*0.35) + Salario
+Total_Hextras = Horas_extras*valor_hora
